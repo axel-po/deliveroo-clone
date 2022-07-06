d71f46125345ffb35528e5bad7165c029e809c19
Axel Po <axelpoin@gmail.com>
2022-07-06 11:17:39 +0200

    feat: form login add react-hook-form

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,9 +3,25 @@ import { useState } from "react";
 import { Button } from "../components/Buttons/Buttons";
 import Footer from "../components/Footer/Footer";
 import Nav from "../components/Nav/Nav";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  email: string;
+  exampleRequired: string;
+};
 
 export default function login() {
   const [click, setClick] = useState<boolean>(false);
+
+  const { register, handleSubmit, formState, setError } = useForm<Inputs>({
+    mode: "onTouched",
+  });
+
+  const { isSubmitting, isValid } = formState;
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data, isValid);
+  };
 
   return (
     <>
@@ -15,7 +31,7 @@ export default function login() {
           <h2 className='font-bold text-2xl mb-6'>Inscription ou connexion</h2>
 
           {click == true ? (
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
               <label htmlFor='email'>Adresse email</label>
               <div className='flex flex-col gap-3 my-[10px]'>
                 <input
@@ -23,8 +39,11 @@ export default function login() {
                   type='email'
                   id='email'
                   placeholder='par ex. elonmusk@aws.com'
+                  {...register("email")}
                 />
-                <Button>Continuer</Button>
+                <button disabled={isValid == true ? false : true} className='bg-green  text-white font-semibold rounded py-[12px] px-[24px]'>
+                  Continuer
+                </button>
               </div>
             </form>
           ) : (
