c0853e9f13b2400b20d0dfd0b924377eaf6d4b40
Axel Po <axelpoin@gmail.com>
2022-07-23 16:36:50 +0200

    add foodCat

--- components/CardCategoryFood/CardCategoryFood.tsx
+++ components/CardCategoryFood/CardCategoryFood.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+type Props = {
+  title: string;
+  imageUrl: string;
+};
 
-export default function CardCategoryFood() {
+export default function CardCategoryFood({ title, imageUrl }: Props) {
   return (
-    <div className="flex w-[170px] h-[80px] rounded bg-cover cursor-pointer bg-center bg-[url('https://co-restaurants.roocdn.com/images/c43f547c8863247dab3224cc985a0610f6cb6a80/shortcut/burgers-2.png?width=334&height=176&fit=crop&bg-color=00ccbc&auto=webp&format=png')]">
-      <h5 className='text-white text-lg mt-auto mb-2 ml-2'>Burgers</h5>
+    <div style={{ backgroundImage: `url(${imageUrl})` }} className={`flex w-[170px] h-[80px] rounded bg-cover cursor-pointer bg-center `}>
+      <h5 className='text-white text-lg mt-auto mb-2 ml-2'>{title}</h5>
     </div>
   );
 }
--- pages/restaurants/index.tsx
+++ pages/restaurants/index.tsx
@@ -1,8 +1,13 @@
 import Nav from "../../components/Nav/Nav";
 import { TitlePageMenu } from "../../components/Typography/Typography";
 import CardRestaurants from "../../components/CardRestaurants/CardRestaurants";
+import { URL_GET_CATEGORY } from "../../config/config";
+import CardCategoryFood from "../../components/CardCategoryFood/CardCategoryFood";
+import { Key } from "react";
 
-export default function Restaurants() {
+export default function Restaurants({ foodCategories }: any) {
+  console.log(foodCategories);
+
   return (
     <>
       <Nav page={{ page: "menu" }} />
@@ -14,7 +19,11 @@ export default function Restaurants() {
         <main className='px-[30px]'>
           <TitlePageMenu>Livraison dans le quartier Reims - Centre Ville</TitlePageMenu>
 
-          <div className='flex flex-wrap gap-3'></div>
+          <div className='flex flex-wrap gap-3'>
+            {foodCategories.map((foodCat: { _id: Key | null | undefined; title: string; imageUrl: string }) => (
+              <CardCategoryFood key={foodCat?._id} title={foodCat?.title} imageUrl={foodCat?.imageUrl} />
+            ))}
+          </div>
 
           <TitlePageMenu>À la une</TitlePageMenu>
           <div className='grid gap-3 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5'>
@@ -30,4 +39,15 @@ export default function Restaurants() {
       </div>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const data = await fetch(URL_GET_CATEGORY);
+  const foodCategories = await data.json();
+
+  return {
+    props: {
+      foodCategories,
+    },
+  };
 }
