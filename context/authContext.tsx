d81548a644c9e485b63234975de5d25b555754b5
Axel Po <axelpoin@gmail.com>
2022-07-24 16:22:56 +0200

    fix

--- context/authContext.tsx
+++ context/authContext.tsx
@@ -1,6 +1,5 @@
 import { createContext, useContext } from "react";
 import { useState } from "react";
-// import { isAuthticated } from "../utils/api";
 
 type AuthContextType = {
   isAuth: boolean;
